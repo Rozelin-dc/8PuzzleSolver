ba28ef59311b1027b60222bf2759d87decab84a1
Rozelin <66714866+Rozelin-dc@users.noreply.github.com>
2021-06-21 17:27:16 +0900

    強制終了ボタン追加

---
+++ Assets/Scripts/StopButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopButton : MonoBehaviour
+{
+    public static bool IsButtonDowned = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        IsButtonDowned = true;
+    }
+
+    public static bool GetIsButtonDowned()
+    {
+        return IsButtonDowned;
+    }
+
+    public static void ResetIsButtonDowned()
+    {
+        IsButtonDowned = false;
+    }
+}
