c7db84fe21581bd9b073ba4d76a721700e33ff8a
Rozelin <66714866+Rozelin-dc@users.noreply.github.com>
2021-06-21 16:52:58 +0900

    時間管理追加

--- Assets/Scripts/CurrentState.cs
+++ Assets/Scripts/CurrentState.cs
@@ -8,15 +8,15 @@ public class CurrentState : MonoBehaviour
     public static char[,] CurrentGrid = new char[,] { {'2', '5', '8' }, {'3', ' ', '7' }, {'1', '6', '4' } };
     public static int[] BlankCoordinate = new int[] {2, 2 };
 
-    public GameObject Text00;
-    public GameObject Text01;
-    public GameObject Text02;
-    public GameObject Text10;
-    public GameObject Text11;
-    public GameObject Text12;
-    public GameObject Text20;
-    public GameObject Text21;
-    public GameObject Text22;
+    public Text Text00;
+    public Text Text01;
+    public Text Text02;
+    public Text Text10;
+    public Text Text11;
+    public Text Text12;
+    public Text Text20;
+    public Text Text21;
+    public Text Text22;
 
     // Start is called before the first frame update
     void Start()
@@ -27,15 +27,15 @@ public class CurrentState : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Text00.GetComponent<Text>().text = CurrentGrid[0, 0].ToString();
-        Text01.GetComponent<Text>().text = CurrentGrid[0, 1].ToString();
-        Text02.GetComponent<Text>().text = CurrentGrid[0, 2].ToString();
-        Text10.GetComponent<Text>().text = CurrentGrid[1, 0].ToString();
-        Text11.GetComponent<Text>().text = CurrentGrid[1, 1].ToString();
-        Text12.GetComponent<Text>().text = CurrentGrid[1, 2].ToString();
-        Text20.GetComponent<Text>().text = CurrentGrid[2, 0].ToString();
-        Text21.GetComponent<Text>().text = CurrentGrid[2, 1].ToString();
-        Text22.GetComponent<Text>().text = CurrentGrid[2, 2].ToString();
+        Text00.text = CurrentGrid[0, 0].ToString();
+        Text01.text = CurrentGrid[0, 1].ToString();
+        Text02.text = CurrentGrid[0, 2].ToString();
+        Text10.text = CurrentGrid[1, 0].ToString();
+        Text11.text = CurrentGrid[1, 1].ToString();
+        Text12.text = CurrentGrid[1, 2].ToString();
+        Text20.text = CurrentGrid[2, 0].ToString();
+        Text21.text = CurrentGrid[2, 1].ToString();
+        Text22.text = CurrentGrid[2, 2].ToString();
     }
 
     public static void Shuffle()
---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    public static Stopwatch sw = new System.Diagnostics.Stopwatch();
+
+    public Text Text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Text.text = "実行時間: " + (sw.ElapsedMilliseconds / 1000) + "[sec]";
+    }
+
+    public static void ResetTimer()
+    {
+        sw.Restart();
+    }
+
+    public static void StartTimer()
+    {
+        sw.Start();
+    }
+
+    public static void StopTimer()
+    {
+        sw.Stop();
+    }
+}
